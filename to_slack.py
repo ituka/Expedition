84c893069d57eabe8ce5a052d1212b487b3f8ea5
Masatoshi Shigemitsu <itukakuroi@gmail.com>
2016-11-08 16:08:10 +0900

    name enc

--- to_slack.py
+++ to_slack.py
@@ -3,4 +3,5 @@ import slackweb
 tmp = input("何？")
 
 slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
-slack.notify(text="%s" %(tmp))
+slack.notify(text="%s" %(tmp), channel="#test_post",
+             username="kaga", mrkdwn=True)
